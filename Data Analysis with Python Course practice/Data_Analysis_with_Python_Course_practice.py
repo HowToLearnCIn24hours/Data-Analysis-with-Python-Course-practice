e79eeacef8d2825c800a84e96a0be5181e03006b
Kat <KatieTheLibra@protonmail.com>
2023-05-31 22:52:35 +0300

    Add project files.

---
+++ Data Analysis with Python Course practice/Data_Analysis_with_Python_Course_practice.py
@@ -0,0 +1,21 @@
+
+my_wife_name = 'Natasha   '
+my_wife_age = 23;
+my_name = 'Kat';
+my_age = 24;
+age_difference = my_age-my_wife_age;
+print('This is the age difference between me and my wife',age_difference);
+age_difference>=1;
+print(my_age<25 or my_wife_age>25);
+print(my_age<25 and my_wife_age>25);
+my_fav_food, wife_fav_food = 'ramen', 'burger';
+my_fav_music=wife_fav_music = 'pop';
+print(my_fav_music,wife_fav_music);
+wife_fav_music = 'electro';
+print(wife_fav_music);
+my_age = my_age+1;
+print(my_name[1])
+print('Nat' in my_wife_name)
+sentence = my_name +' ' + 'and' + ' ' + my_wife_name + ' ' + '=love'
+print(sentence.lower(), my_fav_food.upper(), wife_fav_music.capitalize())
+print(sentence.strip())
